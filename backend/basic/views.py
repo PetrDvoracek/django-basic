18da6a85a0e51a3f258d2c351301fe2a3c1e44c7
pedro <pedro.dvoracek@gmail.com>
2019-02-09 22:28:17 +0100

    work on REST API

--- backend/basic/views.py
+++ backend/basic/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import DataSerializer
+from .models import Data
+
+class DataViewSet(viewsets.ModelViewSet):
+    queryset = Data.objects.all()
+    serializer_class = DataSerializer
 
 # Create your views here.
