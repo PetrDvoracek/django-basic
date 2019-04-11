62423666e1a22e77bb7fa9c78d1188003ef82a16
pedro <pedro.dvoracek@gmail.com>
2019-04-11 17:39:32 +0200

    opravena chyba nacitani dat, novy template html

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -20,5 +20,5 @@ from basic import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
-    path('api/get_time_serie', views.get_time_serie)
+#    path('api/get_time_serie', views.get_time_serie)
 ]
